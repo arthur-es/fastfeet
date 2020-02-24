f17053f1e3a814f01868cfce0966c8e50c3e01e8
arthur-es <arthurcastroes@gmail.com>
2020-02-24 16:07:17 -0300

    feature: update deliveryman

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -40,7 +40,38 @@ class DeliverymanController {
     return res.json(deliveryManCreated);
   }
   async update(req, res) {
-    return res.json({ ok: 'ko' });
+    const { id } = req.params;
+    if (!id)
+      return res
+        .status(400)
+        .json({ error: 'You must provide a param of `id`' });
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string(),
+      avatar_id: Yup.number(),
+    });
+
+    const validationResult = await schema
+      .validate(req.body, {
+        abortEarly: false,
+      })
+      .catch(err => {
+        yupValidationErrors = err.errors;
+      });
+
+    if (!validationResult) {
+      return res.status(400).json({
+        error: 'Yup validation failed',
+        errors: yupValidationErrors,
+      });
+    }
+
+    const deliveryMan = await Deliveryman.findByPk(id);
+
+    const updatedDeliveryman = await deliveryMan.update(req.body);
+
+    return res.json(updatedDeliveryman);
   }
 
   async delete(req, res) {
