dced4b4a4a2ad6422ba97d3aabd3c3c42b3352df
arthur-es <arthurcastroes@gmail.com>
2020-02-24 15:38:26 -0300

    installing and config of multer

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
+import multer from 'multer';
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 import logRequests from './app/middlewares/logRequests';
+
+import multerConfig from './config/multer';
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -27,5 +32,7 @@ routes.get('/deliverymen', DeliverymanController.index);
 routes.post('/deliverymen', DeliverymanController.store);
 routes.put('/deliverymen', DeliverymanController.update);
 routes.delete('/deliverymen', DeliverymanController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
